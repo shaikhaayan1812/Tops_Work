53bebb54378480254bd8adbb5a2dd737d3c058c2
Ayan Shaikh <shaikhaayan1812@gmail.com>
2026-09-20 11:45:58 +0000

    Enhance calculator to handle multiple items
    Updated the calculator to calculate bill based on item selection.

--- assesment/Bacis_py/Session_2/Calculator.py
+++ assesment/Bacis_py/Session_2/Calculator.py
@@ -2,10 +2,18 @@ apple = 50
 banana = 20
 mango = 10
 
-select=int(input("Enter the contyty"))
 
-item = input("Enter the item name")
+select = int(input("Enter the quantity: "))
+item = input("Enter the item name: ")
 
-bill = select * item
+if item == "apple":
+    bill = select * apple
+elif item == "banana":
+    bill = select * banana
+elif item == "mango":
+    bill = select * mango
+else:
+    bill = 0
+    print("Item not available")
 
-print(bill)
+print("Your bill is:", bill)
